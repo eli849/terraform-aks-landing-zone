a458900805d33f3a16622f6dedf368b925fbb054
eli849 <100967367+eli849@users.noreply.github.com>
2025-10-16 16:54:46 -0400

    Add files via upload

--- landing-zone/providers.tf
+++ landing-zone/providers.tf
@@ -26,18 +26,9 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
 
-provider "azuread" {
-  client_id     = var.client_id
-  client_secret = var.client_secret
-  tenant_id     = var.tenant_id
-}
+provider "azuread" {}
 
 
 provider "helm" {}
--- landing-zone/terraform.tfvars
+++ landing-zone/terraform.tfvars
@@ -3,7 +3,4 @@ location = "westus"
 sp_name = "aks-service-principal"
 vnet_cidr = ["10.0.0.0/16"]
 subnet_cidr = ["10.0.1.0/24"]
-subscription_id = ""
-client_id       = ""
-client_secret   = ""
-tenant_id       = ""
+
